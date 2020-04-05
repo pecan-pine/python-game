e39fd34ed44092e042ab2b7004063b2eed20245f
pecan-pine <aquilegia.taylor2@gmail.com>
2020-04-05 11:55:37 -0700

    Uploading game

---
+++ settings.py
@@ -0,0 +1,21 @@
+import pygame, time,random
+
+# Import pygame.locals for easier access to key coordinates
+# Updated to conform to flake8 and black standards
+from pygame.locals import (
+    RLEACCEL,
+    K_UP,
+    K_DOWN,
+    K_LEFT,
+    K_RIGHT,
+    K_ESCAPE,
+    K_SPACE,
+    KEYDOWN,
+    QUIT,
+)
+
+
+#SCREEN WIDTH AND HEIGHT
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
